7ba3d1aec0bea48e11fc302ccc85db67471d14cb
Paramasivan-T <paramasivan.22mc@kct.ac.in>
2024-02-28 20:34:08 +0530

    'auth'

--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Ticket(models.Model):
+   issue = models.TextField()
+   created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+   created_at = models.DateTimeField(auto_now = True)
+   associated_project  = models.ForeignKey('Project', on_delete=models.CASCADE)
+   status = models.BooleanField()
+
+
+class Project(models.Model):
+   name = models.CharField(max_length = 50)
+   description = models.TextField()
+   devs = models.ManyToManyField(User)
+   image = models.ImageField(upload_to='images')
+
+
+class ResolvedTicket(models.Model):
+   issue = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+   solution = models.TextField()
+   solved_by = models.ForeignKey(User, on_delete=models.CASCADE)
+   sloved_at = models.DateTimeField(auto_now = True)
+
+
+class Log(models.Model):
+   issue = models.ForeignKey('Ticket', on_delete=models.CASCADE)
+   solution = models.ForeignKey('ResolvedTicket', on_delete=models.CASCADE)
+
+
+   
+
+
+
--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def home(request):
+    return render(request, 'ticket/home.html')
+
+
+def CreateUser(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Successfully created for {username}')
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'ticket/signup.html',context=context)
+
+
+def LoginUser(request):
+    if request.method == 'POST':
+        request.POST.get('username')
+        request.POST.get('password')
+        # user = authenticate(request, username=username)
+
+    return render(request, 'ticket/login.html')
+
